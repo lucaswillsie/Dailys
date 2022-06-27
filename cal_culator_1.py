c8ae523bd70d7ba0e19edbd38d7789f505d5a8e9
Lucas Willsie <60862306+lucaswillsie@users.noreply.github.com>
2022-06-27 17:57:41 -0500

    Update cal_culator_1.py

--- cal_culator_1.py
+++ cal_culator_1.py
@@ -1,13 +1,10 @@
 days = range(365)
 calorie = input("Daily Cal: ")
-weight = input("Weight: ")
-dif = float(calorie) - (float(weight) * 10)
-pound = dif / 3600
+weight = int(input("Weight: "))
 
 for day in days:
     print(f"Day - {day + 1} ")
-    try:
-        weight = float(weight) + float(pound)
-    except:
-        pass
+    dif = float(calorie) - (float(weight) * 10)
+    pound = dif / 3600
+    weight = float(weight) + float(pound)
     print(weight + (dif / 3600))
