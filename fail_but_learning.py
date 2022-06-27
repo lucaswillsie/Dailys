67c76318b1d8670c1ea9f7e3c1fe76758f847288
Lucas Willsie <60862306+lucaswillsie@users.noreply.github.com>
2022-06-26 23:57:54 -0500

    Create fail_but_learning.py

---
+++ fail_but_learning.py
@@ -0,0 +1,14 @@
+change = {"hundred": 100, "fifty": 50, "twenty": 20, "ten": 10, "five": 5, "one": 1, "half_dollar": .50, "quarter": .25, "dime": .10, "nickle": .05, "penny": .01}
+
+hundred_ask = input(f"how many {change[100]} do you have? ")
+fifty_ask = input(f"how many {change[50]} do you have? ")
+twenty_ask = input(f"how many {change[20]} do you have? ")
+ten_ask = input(f"how many {change[10]} do you have? ")
+five_ask = input(f"how many {change[5]} do you have? ")
+one_ask = input(f"how many {change[1]} do you have? ")
+half_dollar_ask = input(f"how many {change[.50]} do you have? ")
+quarter_ask = input(f"how many {change[.25]} do you have? ")
+dime_ask = input(f"how many {change[.10]} do you have? ")
+nickle_ask = input(f"how many {change[.05]} do you have? ")
+penny_ask = input(f"how many {change[.01]} do you have? ")
+
